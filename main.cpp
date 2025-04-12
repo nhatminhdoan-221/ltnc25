da3768342e5e66f3ddb426e48a0e318503cf3e68
nhatminhdoan-221 <24020228@vnu.edu.vn>
2025-04-12 11:37:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
+#define SDL_MAIN_HANDLED  
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_ttf.h>
 #include "game.h"
+#include "constants.h"
+#include "GameUI.h"
 
-int main(int argc, char* argv[]) {
-    const int FPS = 60;
-    const int frameDelay = 1000 / FPS;
 
-    Uint32 frameStart;
-    int frameTime;
+int main() {
+	Game* game = new Game();
 
-    Game* game = new Game();
+	game->Init();
 
-    if (game->init("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, false)) {
-        while (game->running()) {
-            frameStart = SDL_GetTicks();
+	while (game->Running()) {
+		Uint32 frameStart = SDL_GetTicks();
+		int frameTime;
 
-            game->handleEvents();
-            game->update();
-            game->render();
+		game->HandleEvents();
+		game->Update();
+		game->Render();
 
-            frameTime = SDL_GetTicks() - frameStart;
-            if (frameDelay > frameTime) {
-                SDL_Delay(frameDelay - frameTime);
-            }
-        }
-    }
+		frameTime = SDL_GetTicks() - frameStart;
 
-    delete game;
-    return 0;
+		if (frameTime < FRAME_DELAY) {
+			SDL_Delay(FRAME_DELAY - frameTime);
+		}
+	}
+
+	game->Clean();
+	return 0;
 }
+
