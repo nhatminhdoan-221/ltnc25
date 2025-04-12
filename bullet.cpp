ce196aaff9633efe71a8807b959ea7759ea5df60
nhatminhdoan-221 <24020228@vnu.edu.vn>
2025-04-12 11:34:09 +0700

    Delete bullet.cpp

--- bullet.cpp
+++
@@ -1,8 +0,0 @@
-#include "bullet.h"
-#include "game.h"
-
-Bullet::Bullet(const char* textureSheet, int x, int y) : GameObject(textureSheet, x, y), m_speed(-10) {}
-
-void Bullet::update() {
-    m_y += m_speed;
-}
